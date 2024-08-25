6cbef263fe11f19dd88c2b7bb19bcb9bca852bef
XingJinming-real <xjm0801@126.com>
2024-08-24 21:38:39 -0400

    update test_main.py

--- test_main.py
+++ test_main.py
@@ -5,8 +5,8 @@ def test_pass():
     assert 1 + 1 == 2  # This test will pass
 
 
-def test_fail():
-    assert 1 + 1 == 3  # This test will fail
+# def test_fail():
+#     assert 1 + 1 == 3  # This test will fail
 
 
 # def test_sample():
