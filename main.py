d416563cdee5fbb254780274c995cf3187b32fde
XingJinming-real <xjm0801@126.com>
2024-08-24 20:43:57 -0400

    add code coverage badge

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ def main():
     a = [1, 2, 3]
     for i in range(len(a)):
         a[i] += 1  # add each element by 1
-    print(a[3])  # error will happen here as the index out of range
+    # print(a[3])  # error will happen here as the index out of range
     return a[0]
 
 
--- test_main.py
+++ test_main.py
@@ -1,6 +1,13 @@
+from main import main
+
+
 def test_pass():
     assert 1 + 1 == 2  # This test will pass
 
 
 def test_fail():
     assert 1 + 1 == 3  # This test will fail
+
+
+def test_sample():
+    assert main() == 2
