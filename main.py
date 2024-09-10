cbc5996d9de9b56d6d8a5ce03089abcdd516e636
XingJinming-real <xjm0801@126.com>
2024-09-10 12:03:29 -0400

    add codes docstrings, update coverage badge

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
+"""example codes for homework 1"""
 def main():
+    """example codes for hw1
+
+    Returns:
+        list: list of ints
+    """
     a = [1, 2, 3]
-    for i in range(len(a)):
+    for i,_ in enumerate(a):
         a[i] += 1  # add each element by 1
     # print(a[3])  # error will happen here as the index out of range
     return a[0]
--- test_main.py
+++ test_main.py
@@ -1,7 +1,10 @@
+""" test codes for hw1 """
 from main import main
 
 
 def test_pass():
+    """example test codes, this one will always pass
+    """
     assert 1 + 1 == 2  # This test will pass
 
 
@@ -9,5 +12,7 @@ def test_pass():
 #     assert 1 + 1 == 3  # This test will fail
 
 
-def test_main(): #This tests will also pass, HW2
+def test_main():  # This tests will also pass, HW2
+    """test codes, judge if the return value of main would be 2
+    """
     assert main() == 2
