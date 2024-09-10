957fe7da3a55c8c5f96728cc7355ee41ce6c7f67
Shreyas Devaraj <shreyasdevaraj@Shreyass-MBP.lan>
2024-09-10 00:31:15 -0400

    pass hw1 tests'

--- test_main.py
+++ test_main.py
@@ -9,5 +9,5 @@ def test_pass():
 #     assert 1 + 1 == 3  # This test will fail
 
 
-def test_main(): #This tests the return value of the main function, will fail
-    assert main() == 3
+def test_main(): #This tests will also pass, HW2
+    assert main() == 2
