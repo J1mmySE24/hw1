cade0ceffd7faf5e035e76c1c994be8b5c9bcd73
Shreyas Devaraj <57234787+shreyasdvrj@users.noreply.github.com>
2024-10-09 16:50:17 +0000

    change order of sed

--- hw4/task3.sh
+++ hw4/task3.sh
@@ -4,7 +4,7 @@
 # Step 2: Replace gender labels in the dataset
 # Step 3: Calculate the average age of filtered passengers
 gawk -F, 'NR > 1 && $3 == 2 && $13 ~ /S/ \
- { print | "sed -e \"s/male/M/; s/female/F/\" >> filtered_passengers.csv"; if ($7 != "") { total_age += $7; count++ } } END { if (count > 0) \
+ { print | "sed -e \"s/female/F/; s/male/M/\" >> filtered_passengers.csv"; if ($7 != "") { total_age += $7; count++ } } END { if (count > 0) \
  printf "Average Age: %.2f\n", total_age / count; else print "No passengers found." }' titanic.csv
 
 read -p "Press any key to continue"
