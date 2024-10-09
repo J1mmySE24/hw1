0bd9d0ef4b78ce56ac66f0c7a9c47113e448b3a9
krishapatel10 <99911043+krishapatel10@users.noreply.github.com>
2024-10-09 00:13:12 -0400

    Update task3.sh

--- hw4/task3.sh
+++ hw4/task3.sh
@@ -2,9 +2,11 @@
 
 # Step 1: Filter passengers from the Titanic dataset
 gawk -F, 'NR > 1 && $3 == 2 && ($13 ~/S/)' titanic.csv
-# Step 2: Replace gender labels in the dataset
+
+# Step 2: Replace gender labels MALE -> M and female -> to F in the dataset
 echo "Updated gender labels in the dataset:"
 sed 's/,male/,M/g; s/,female/,F/g' titanic.csv
+
 # Step 3: Calculate the average age of filtered passengers
 gawk -F, 'NR > 1 && $3 == "2" && ($13 ~ /S/) { if ($7 != "") { total_age += $7; count++ } } 
           END { 
